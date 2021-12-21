55afc936a98642b865a380caecf8202a6d8f5c00
“kyleisme14” <kylekallman@gmail.com>
2021-12-20 19:45:56 -0500

    tictac

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+//import componeensts
+
+import Game from './Game';
+
+class App extends Component {
+
+    render() {
+        return(
+            <div className='App'>
+            <Game />
+            </div>
+        );
+    }
+}
+
+export default App;
